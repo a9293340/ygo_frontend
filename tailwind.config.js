23c529c7cd4f212ca0d2bd0949a863c62576cf39
EricHong <f102041332@gmail.com>
2023-04-17 17:24:57 +0800

    [plugin] vue-chart-3 & tailwindcss-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('flowbite/plugin')],
+	content: [
+		'./index.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./node_modules/flowbite/**/*.js',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin'),
+		require('tailwind-scrollbar')({ nocompatible: true }),
+	],
 };
