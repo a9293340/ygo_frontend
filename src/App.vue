59d9255f041bfd85dfaec8f6b035ee989b952b62
alex1234567639 <alex8603062000@gmail.com>
2023-10-14 21:07:58 +0800

    [feature]新增footer component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div class="app">
     <Navbar/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '/src/components/common/Navbar.vue'
+import Footer from '/src/components/common/Footer.vue'
 </script>
 
 <style lang="scss" scoped>
