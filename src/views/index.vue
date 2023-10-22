cb497408ceaf9055650e8ca0b3db175bd9275059
alex1234567639 <alex8603062000@gmail.com>
2023-10-22 15:21:17 +0800

    [feature]新增首頁文章列表component

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="index-wrapper">
-    index-wrapper
+    <Banner/>
+    <TopArticle
+      :title="$t('menu.meta_deck')"
+      :type="0"
+      :router="'/meta_deck'"
+    />
+    <TopArticle
+        :title="$t('menu.series_introduction')"
+        :type="1"
+        :router="'/series_introduction'"
+    />
+    <TopArticle
+        :title="$t('menu.useful_card_introduction')"
+        :type="2"
+        :router="'/useful_card_introduction'"
+    />
   </div>
 </template>
 
+<script setup lang="ts">
+import Banner from '/src/components/home/Banner.vue'
+import TopArticle from '/src/components/home/TopArticle.vue'
+
+</script>
+
 <style lang="scss" scoped>
 .index-wrapper {
-
 }
 </style>
