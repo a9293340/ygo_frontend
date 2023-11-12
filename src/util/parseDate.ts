ac8742beab30dff10ecfb84f4417f3d070609ad5
alex1234567639 <alex8603062000@gmail.com>
2023-11-12 20:32:41 +0800

    [fetaure]新增formatMD

--- src/util/parseDate.ts
+++ src/util/parseDate.ts
@@ -4,5 +4,14 @@ export function formatDateString(inputDateString: string): string {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
 
-    return `${year}-${month}-${day}`;
+    return `${year}/${month}/${day}`;
+}
+
+export function formatMD(inputDateString: string): string {
+    const date = new Date(inputDateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${month}/${day}`;
 }
