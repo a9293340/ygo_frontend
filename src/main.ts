24258e9a617ff18b820eebb21b8638ab0602d19b
Eric Hong <f102041332@gmail.com>
2023-11-16 23:00:45 +0800

    [feature] Deck list 缺EDIT跟DETAIL功能

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import VueClientRecaptcha from "vue-client-recaptcha";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import VueClientRecaptcha from 'vue-client-recaptcha';
 
-import ElementPlus from "element-plus";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import "element-plus/dist/index.css";
+import ElementPlus from 'element-plus';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import 'element-plus/dist/index.css';
 
-import i18n from "@/i18n";
-import VCalendar from "v-calendar";
+import i18n from '@/i18n';
+import VCalendar from 'v-calendar';
 
-import "v-calendar/style.css";
+import 'default-passive-events';
 
-import "./style.css";
+import 'v-calendar/style.css';
+
+import './style.css';
 
 const app = createApp(App);
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-	app.component(key, component);
+  app.component(key, component);
 }
 
 app.use(createPinia());
@@ -26,6 +28,6 @@ app.use(router);
 app.use(i18n);
 app.use(ElementPlus);
 app.use(VCalendar, {});
-app.component("VueClientRecaptcha", VueClientRecaptcha);
+app.component('VueClientRecaptcha', VueClientRecaptcha);
 
-app.mount("#app");
+app.mount('#app');
