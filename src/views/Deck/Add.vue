56bc9527f9f9febf436e8ac8bdd71d0f6aedcf14
Eric <f102041332@gmail.com>
2023-12-11 11:31:21 +0800

    [fixed] true deck build error

--- src/views/Deck/Add.vue
+++ src/views/Deck/Add.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import type { CardsList } from 'module-types';
-import type { CardListType } from 'request-data-types';
-import type { HasTotalRes } from 'response-data-types';
+import type { CardsList } from "module-types";
+import type { CardListType } from "request-data-types";
+import type { HasTotalRes } from "response-data-types";
+import DeckWin from "@/components/Deck/DeckWin.vue";
 
 const cardsList = ref<[] | CardsList>([]);
 const limit = ref(20);
@@ -9,37 +10,44 @@ const page = ref(0);
 const trigger = ref(0);
 const total = ref(0);
 
-const getCardsList = (cards: { cards: HasTotalRes<CardsList>; listQuery: CardListType }) => {
-  if (!cards.listQuery.page) {
-    cardsList.value = [];
-    trigger.value++;
-  }
-  cardsList.value = [...cardsList.value, ...cards.cards.list];
-  total.value = cards.cards.total;
-  page.value = cards.listQuery.page;
+const getCardsList = (cards: {
+	cards: HasTotalRes<CardsList>;
+	listQuery: CardListType;
+}) => {
+	if (!cards.listQuery.page) {
+		cardsList.value = [];
+		trigger.value++;
+	}
+	cardsList.value = [...cardsList.value, ...cards.cards.list];
+	total.value = cards.cards.total;
+	page.value = cards.listQuery.page;
 };
 
 const callApi = () => {
-  page.value = page.value + 1;
+	page.value = page.value + 1;
 };
 </script>
 
 <template>
-  <div class="deck-add">
-    <SearchCards @get:data="getCardsList" v-model:page="page" v-model:limit="limit" />
-    <div class="main-win">
-      <DeckWin
-        :total="total"
-        :cards-list.sync="cardsList"
-        @call:api="callApi"
-        :trigger.sync="trigger"
-      />
-    </div>
-  </div>
+	<div class="deck-add">
+		<SearchCards
+			@get:data="getCardsList"
+			v-model:page="page"
+			v-model:limit="limit"
+		/>
+		<div class="main-win">
+			<DeckWin
+				:total="total"
+				:cards-list.sync="cardsList"
+				@call:api="callApi"
+				:trigger.sync="trigger"
+			/>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .deck-add {
-  min-height: calc(100vh - 104px);
+	min-height: calc(100vh - 104px);
 }
 </style>
