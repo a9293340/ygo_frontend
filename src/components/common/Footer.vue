ae9d900ba7173c9b16672b9a43dae9282b40701b
Eric Hong <f102041332@gmail.com>
2023-12-12 22:34:32 +0800

    [CICD] testing

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer-wrapper">
-    <div class="text">Copyright © 卡壇 YGO Info. All rights reserved.test1</div>
+    <div class="text">Copyright © 卡壇 YGO Info. All rights reserved.</div>
   </div>
 </template>
 
