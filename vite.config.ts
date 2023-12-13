2b4576a44bad83d730ae62e566a791904e53ada5
Eric <f102041332@gmail.com>
2023-12-13 09:15:26 +0800

    [fixed] article router 錯誤

--- vite.config.ts
+++ vite.config.ts
@@ -59,10 +59,11 @@ export default defineConfig({
 		cors: true,
 		proxy: {
 			"/api": {
-				target: "http://172.233.65.87:3300",
+				target: "http://localhost:3300",
 				changeOrigin: true,
 				rewrite: (path) => path,
 			},
 		},
 	},
 });
+// http://172.233.65.87:3300
