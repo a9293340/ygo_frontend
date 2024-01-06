647dfe395a72c19aa9b0e12e19e9d7655501a93f
alex1234567639 <alex8603062000@gmail.com>
2024-01-07 00:07:46 +0800

    [feature]

--- src/auto-components.ts
+++ src/auto-components.ts
@@ -14,6 +14,7 @@ declare module '@vue/runtime-core' {
     DeckDetailComponent: typeof import('./components/Deck/DeckDetailComponent.vue')['default']
     DeckWin: typeof import('./components/Deck/DeckWin.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
